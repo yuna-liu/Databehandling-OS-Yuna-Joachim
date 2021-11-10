9007483053e6c0a9265f98311251fe504667134d
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-11-10 11:14:53 +0100

    Y: to_datetime of year and add time slider to dashboard

--- Q1_dashboard_time_slider.py
+++ Q1_dashboard_time_slider.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from dash import dcc, html
 import dash
 from load_data import ShowMeData
@@ -17,7 +18,10 @@ df_medal = df_canada.pivot(index="Year", columns="Medal", values="ID")
 df_medal.fillna(0, inplace=True)
 df_medal.reset_index(drop=False, inplace=True )
 df_medal["Total"] = df_medal["Bronze"] + df_medal["Gold"] + df_medal["Silver"]
-df_medal = df_medal.astype("int32")
+df_medal= df_medal.astype("int32")
+df_medal['Year']= pd.to_datetime(df_medal['Year'], format='%Y').dt.year
+# pd.to_datetime() generates Year-01-01 00:00:00
+# add .dt.year to show only Year: https://stackoverflow.com/questions/51792903/use-only-year-for-datetime-index-pandas-dataframe
 df_medal.head(10)
 
 # Initiate dashboard
@@ -39,17 +43,33 @@ app.layout = html.Div([
                  min= 1896, max=2016, step=2,
                  dots=True, value=[1896, 2020],
                  marks = slider_marks),
-    dcc.Store(id="filtered-df")
+    
 ])
 
 
 @app.callback(Output("medals-graph", "figure"),
-              Input("medal-picker-dropdown", "value")
+              Input("medal-picker-dropdown", "value"),
+              Input("time-slider", "value")
               )
               
-def update_graph(medal):
+def update_graph(medal, time_index):
+    # time_index is a list of two points choosen by user
+    # the left point refers to time_index[0]
+    # the right point refers to time_index[1]
+    # choose accordingly a subset of dataframe
+    # This is inspired by "Create Dashboard in Plotly Dash with dependent drop down list (chained callbacks) and range slider":
+    # https://www.youtube.com/watch?v=TsYwhX0hEA8&t=244s
     
-    fig = px.bar(df_medal, x="Year", y=medal, title=f"The number of {medal}s from Athens 1896 to Rio 2016")
+    dff = df_medal[(df_medal["Year"]>=time_index[0]) & (df_medal["Year"]<=time_index[1])]
+    fig = px.bar(dff, x="Year", y=medal, title=f"The number of {medal}s from {time_index[0]} to {time_index[1]}")
+
+    for data in fig.data:
+        data["width"]= 0.5
+    # when user choose a time_index to a small range, t.ex, 5 years,
+    # the bar width is so large that spread to the year before and the year after
+    # the bar width need to be smaller
+    # http://www.programshelp.com/help/python/Increase__bar_width___px_bar_.html
+
     
     return fig
 
