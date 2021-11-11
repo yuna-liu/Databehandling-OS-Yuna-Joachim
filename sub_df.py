e3c01704587c56ba2029f3129bd4fd91101b5230
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-11-11 15:31:38 +0100

    Q2 done and figures saved in visualisering folder

--- sub_df.py
+++ sub_df.py
@@ -4,44 +4,6 @@
 import pandas as pd
 
 
-# Functions
-
-def count_medals(df_orig, column_name):
-    """
-    Gives back number of medals per any attribute
-
-    Input:
-        df_orig: DataFrame
-        column_name: column to get number of medals of each
-    
-    Returns:
-        df_best: new DataFrame
-    """
-    
-    # Remove redundant columns first
-    df_medals = df_orig.drop(
-        [column for column in df_orig.columns if (column != "Medal") and (column != column_name)],
-        axis="columns"
-    )
-
-    # Remove all NaN (no medal won == NaN)
-    df_medals = df_medals.dropna(axis="rows")
-
-    # Add a counter-columns for number of ALL medals
-    df_medals["Number medals"] = 1
-
-    # Add counters of Gold, silver, and bronze medals
-    for medal in df_medals["Medal"].unique():
-        df_medals[f"Number {medal}"] = 0
-        df_medals[f"Number {medal}"].loc[df_medals["Medal"] == medal] = 1
-
-    # Groupby chosen column_name and count all number of medals
-    df_medals = df_medals.groupby(column_name).sum()
-
-    # Give back new dataframe
-    return df_medals
-
-
 # count medals fct with arbitrary number of arguments
 def count_medals_n(df_orig, *arg):
     """
@@ -80,8 +42,8 @@ def count_medals_n(df_orig, *arg):
     # generate Total, avoid using for-loop in pandas dataframe
     df_medals["Total"] = df_medals["Bronze"] + df_medals["Gold"] + df_medals["Silver"]   
     
-    # change type
-    df_medals = df_medals.astype("int64")
+    # change type and change indices to columns
+    df_medals = df_medals.astype(int)
 
     # sort medel descending
     df_medals = df_medals.sort_values("Total", ascending=False)
