0a4ade4f144f3d0439cf4d72ca9004a0380122ad
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-11-11 09:14:34 +0100

    simply the count medals functions with 1, 2, 3 args

--- analyze_functions.py
+++ analyze_functions.py
@@ -43,9 +43,9 @@ def count_medals(df_orig, column_name):
 
 
 
-# Functions count_medals_test does not produce warning
+# Count medals for one arg
 
-def df_count_medals(df_orig, column_name):
+def count_medals_1(df_orig, column_name):
     """
     Gives back number of medals per any attribute
 
@@ -89,16 +89,14 @@ def df_count_medals(df_orig, column_name):
 
 
 
-
-
-# def function to count medals by year 
-def count_medals_by_year(df_orig, column_name=None):
+# count medals fct with two arguments
+def count_medals_2(df_orig, arg1, arg2):
     """
-    Gives back number of medals per any attribute
+    Gives back number of medals groupby several attributes: arg1, arg2
 
     Input:
         df_orig: DataFrame
-        column_name: column to get number of medals of each
+        arg1, arg2: column to get number of "Medal"
     
     Returns:
         df_best: new DataFrame
@@ -107,15 +105,15 @@ def count_medals_by_year(df_orig, column_name=None):
     df_medals = df_orig[df_orig['Medal'].notna()]
     
     # count medals by column_name
-    df_medals = df_medals.groupby(["Year", column_name,  "Medal"]).count().reset_index()
+
+    df_medals = df_medals.groupby([arg1, arg2, "Medal"]).count().reset_index()
   
     # Remove redundant columns, "Medal" is str column which is one level
     # "ID" column stands for the sum of medals
-
-    df_medals = df_medals.loc[:, ["Year", column_name, "Medal", "ID"]]
+    df_medals = df_medals.loc[:, [arg1, arg2, "Medal", "ID"]]
     
     # data long to wide
-    df_medals = df_medals.pivot(index=["Year", column_name], columns="Medal", values="ID")
+    df_medals = df_medals.pivot(index=[arg1, arg2], columns="Medal", values="ID")
     
     # replace all NAs by 0
     df_medals.fillna(0, inplace=True)
@@ -126,12 +124,6 @@ def count_medals_by_year(df_orig, column_name=None):
     # change type
     df_medals = df_medals.astype("int64")
 
-    # some index are missing, so:
-    df_medals = df_medals.reset_index()
-
-    # set name to index
-    df_medals.index.names = ["ID"]
-    
     # sort medel descending
     df_medals = df_medals.sort_values("Total", ascending=False).head(10)
 
@@ -141,19 +133,14 @@ def count_medals_by_year(df_orig, column_name=None):
 
 
 
-
-
-
-
-
-# def fct for each country
-def count_medals_by_country(df_orig, column_name=None):
+# count medals fct with three arguments
+def count_medals_3(df_orig, arg1, arg2, arg3):
     """
-    Gives back number of medals per any attribute
+    Gives back number of medals groupby several attributes: arg1, arg2, arg3
 
     Input:
         df_orig: DataFrame
-        column_name: column to get number of medals of each
+        arg1, arg2, arg3: column to get number of "Medal"
     
     Returns:
         df_best: new DataFrame
@@ -162,122 +149,15 @@ def count_medals_by_country(df_orig, column_name=None):
     df_medals = df_orig[df_orig['Medal'].notna()]
     
     # count medals by column_name
-    df_medals = df_medals.groupby(["region", column_name,  "Medal"]).count().reset_index()
+
+    df_medals = df_medals.groupby([arg1, arg2, arg3, "Medal"]).count().reset_index()
   
     # Remove redundant columns, "Medal" is str column which is one level
     # "ID" column stands for the sum of medals
-
-    df_medals = df_medals.loc[:, ["region", column_name, "Medal", "ID"]]
+    df_medals = df_medals.loc[:, [arg1, arg2, arg3, "Medal", "ID"]]
     
     # data long to wide
-    df_medals = df_medals.pivot(index=["region", column_name], columns="Medal", values="ID")
-    
-    # replace all NAs by 0
-    df_medals.fillna(0, inplace=True)
-
-    # generate Total, avoid using for-loop in pandas dataframe
-    df_medals["Total"] = df_medals["Bronze"] + df_medals["Gold"] + df_medals["Silver"]   
-    
-    # change type
-    df_medals = df_medals.astype("int64")
-    
-    # some index are missing, so:
-    df_medals = df_medals.reset_index()
-    
-    
-    # set name to index
-    df_medals.index.names = ["ID"]
-
-
-
-    # sort medel descending
-    df_medals = df_medals.sort_values("Total", ascending=False).head(10)
-
-    # Give back new dataframe
-    return df_medals
-
-
-
-
-
-# def fct for each country each year
-def count_medals_by_country_by_year(df_orig, column_name=None):
-    """
-    Gives back number of medals per any attribute
-
-    Input:
-        df_orig: DataFrame
-        column_name: column to get number of medals of each
-    
-    Returns:
-        df_best: new DataFrame
-    """
-    # Remove all NaN (no medal won == NaN)
-    df_medals = df_orig[df_orig['Medal'].notna()]
-    
-    # count medals by column_name
-    df_medals = df_medals.groupby(["Year", "region", column_name,  "Medal"]).count().reset_index()
-  
-    # Remove redundant columns, "Medal" is str column which is one level
-    # "ID" column stands for the sum of medals
-
-    df_medals = df_medals.loc[:, ["Year", "region", column_name, "Medal", "ID"]]
-    
-    # data long to wide
-    df_medals = df_medals.pivot(index=["Year", "region", column_name], columns="Medal", values="ID")
-    
-    # replace all NAs by 0
-    df_medals.fillna(0, inplace=True)
-
-    # generate Total, avoid using for-loop in pandas dataframe
-    df_medals["Total"] = df_medals["Bronze"] + df_medals["Gold"] + df_medals["Silver"]   
-    
-    # change type
-    df_medals = df_medals.astype("int64")
-    
-    # some index are missing, so:
-    df_medals = df_medals.reset_index()
-
-    # set name to index
-    df_medals.index.names = ["ID"]
-
-    
-    # sort medel descending
-    df_medals = df_medals.sort_values("Total", ascending=False).head(10)
-
-    # Give back new dataframe
-    return df_medals
-
-
-
-
-# try def fct with several arguments, but failed
-def count_medals_args(df_orig, *args):
-    """
-    Gives back number of medals per any attribute
-
-    Input:
-        df_orig: DataFrame
-        column_name: column to get number of medals of each
-    
-    Returns:
-        df_best: new DataFrame
-    """
-    # Remove all NaN (no medal won == NaN)
-    df_medals = df_orig[df_orig['Medal'].notna()]
-    
-    # count medals by column_name
-    args_list = list(args)
-    args_list.append("Medal")
-    df_medals = df_medals.groupby(args).count().reset_index()
-  
-    # Remove redundant columns, "Medal" is str column which is one level
-    # "ID" column stands for the sum of medals
-    args_list.append("ID")
-    df_medals = df_medals.loc[:, args_list]
-    
-    # data long to wide
-    df_medals = df_medals.pivot(index=list(args), columns="Medal", values="ID")
+    df_medals = df_medals.pivot(index=[arg1, arg2, arg3], columns="Medal", values="ID")
     
     # replace all NAs by 0
     df_medals.fillna(0, inplace=True)
