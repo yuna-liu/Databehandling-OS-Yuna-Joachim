35178fe810443fee677df369d722a69b4f387355
yuna-liu <89393522+yuna-liu@users.noreply.github.com>
2021-11-15 15:59:25 +0100

    Y: move count_medals with index version in sub_df to af

--- analyze_functions.py
+++ analyze_functions.py
@@ -43,3 +43,53 @@ def count_medals_n(df_orig, *arg):
 
     # Give back new dataframe
     return df_medals
+
+
+
+# count medals fct with arbitrary number of arguments
+# with index and sort_values(by="Total")
+def count_medals(df_orig, *arg):
+    """
+    Gives back number of medals groupby several attributes: *arg
+
+    Input:
+        df_orig: DataFrame
+        *arg: column to get number of "Medal"
+    
+    Returns:
+        df_best: new DataFrame
+    """
+    # Remove all NaN (no medal won == NaN)
+    df_medals = df_orig[df_orig['Medal'].notna()]
+    
+    # count medals by column_name
+
+    args_list = list(arg)
+    args_list.append("Medal")
+    df_medals = df_medals.groupby(args_list).count().reset_index()
+    #df_medals = df_medals.groupby([arg1, arg2, arg3, "Medal"]).count().reset_index()
+  
+    # Remove redundant columns, "Medal" is str column which is one level
+    # "ID" column stands for the sum of medals
+    args_list.append("ID")
+    df_medals = df_medals.loc[:, args_list]
+    
+    # data long to wide
+    args = list(arg)
+    df_medals = df_medals.pivot(index=args, columns="Medal", values="ID")
+    #df_medals = df_medals.pivot(index=[arg1, arg2, arg3], columns="Medal", values="ID")
+
+    # replace all NAs by 0
+    df_medals.fillna(0, inplace=True)
+
+    # generate Total, avoid using for-loop in pandas dataframe
+    df_medals["Total"] = df_medals["Bronze"] + df_medals["Gold"] + df_medals["Silver"]   
+    
+    # change type and change indices to columns
+    df_medals = df_medals.astype(int)
+
+    # sort medel descending
+    df_medals = df_medals.sort_values("Total", ascending=False)
+
+    # Give back new dataframe
+    return df_medals
